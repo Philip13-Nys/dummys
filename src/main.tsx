12a8a019b1954d9e388670f41bb823c3e58b8620
yuichi_13 <miguelphilip246@gmail.com>
2026-07-17 16:13:02 +0800

    fixing coed

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
-import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
-import "./styles/index.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-createRoot(document.getElementById("root")!).render(<App />);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
